692f95f3fad4f61fe06337f139b6360267a6dbe6
Muhammad Saqib <rooott@gmail.com>
2013-11-06 10:56:33 +0500

    routes updated

---
+++ app/controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+class AuthController extends BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function getRegister()
+	{
+		return "registration";
+	}
+
+}
--- app/routes.php
+++ app/routes.php
@@ -10,6 +10,11 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
+/*------------------Authentication Routes---------------------------*/
+	Route::get('user/registration',array('uses'=>'AuthController@getRegister'));
+/*------------------End of Auth Routes------------------------------*/
+
+
 Route::get('dashboard/search',array('uses'=>'DashboardController@getSearch'));
 Route::post('dashboard',array('uses'=>'DashboardController@postIndex'));
 Route::get('dashboard',array('uses'=>'DashboardController@getIndex'));

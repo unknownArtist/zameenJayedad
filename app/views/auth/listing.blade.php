9950d9eb0f1225b59c5168647736f6ece08b6bae
amman <ammanwaheedmughal@gmail.com>
2013-12-17 16:40:22 +0500

    changes

--- app/views/auth/listing.blade.php
+++ app/views/auth/listing.blade.php
@@ -17,8 +17,20 @@ padding-bottom: 10px;
 border-bottom: 1px solid #50b1a8;
 }
 
+tr:nth-child(even) {
+    background-color: #f0f0f0;
+}
+
 
 </style>
+<script type="text/javascript">
+$(document).ready(function()
+{
+  $("tr:even").css("background-color", "#ccc;");
+});
+
+</script>
+
 <div id="wrapper">
 <div id="content">
 

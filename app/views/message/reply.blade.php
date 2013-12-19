0e13a33a503c4aaadaa27409252e161ae26145a3
amman <ammanwaheedmughal@gmail.com>
2013-12-19 15:33:40 +0500

    changes

--- app/views/message/reply.blade.php
+++ app/views/message/reply.blade.php
@@ -1,16 +1,15 @@
 @extends('layouts.login')
 @section('content')
 
-<div id="main-content" class="container-fluid">
+<div id="wrapper">
+<div id="content">
 
-<div id="page-content" class="page-content">
-<div class="row-fluid margin-top20">
-<div class="col-xs-9 grider">
-<div class="widget widget-simple padding-bottom15">
 
-<div class="col-xs-12 widget-header">
-  <h4>Reply Message</h4>
+<div class="compose">
+  <h3>Reply Message</h3>
+  <hr />
 </div>
+	<div id="form-wrapper">
 
    {{ Form::open(array('url'=>'dashboard/message/{id}/reply','POST','files'=>true))}}
    
@@ -23,13 +22,16 @@
 
 <div class="col-xs-12 padding-top15">
 	{{ Form::label('msg', 'Message') }}
-   	{{ Form::textarea('body','',array('class'=>'form-control txtareaheight2')) }}
+   	{{ Form::textarea('body','',array('class'=>'form-control textareaheight')) }}
 
-
-   	{{ HTML::link('user/messages','Return to inbox',array('class'=>'btn btn-success '))}}
-                {{ Form::submit('Send Message',array('class'=>'btn btn-primary pull-right'))}}
+	<div class="btnfloat">
+   	{{ HTML::link('user/messages','Return to inbox',array('class'=>'editbtn '))}}
+                {{ Form::submit('Send Message',array('class'=>'editbtn submtbtn'))}}
+                </div>
                 {{ Form::close() }}
                   </div>
               </div>
               </div>
+              </div>
+              
 @stop
